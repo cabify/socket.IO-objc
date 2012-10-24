e85e54518cadecc0027c0786eab647063c7d5e86
Mark Villacampa <markvjal@gmail.com>
2012-10-24 19:25:52 +0200

    add target to support rubymotion

---
+++ SocketIO/SocketIO.h
@@ -0,0 +1,13 @@
+//
+//  SocketIO.h
+//  SocketIO
+//
+//  Created by Marcos Villacampa on 10/24/12.
+//  Copyright (c) 2012 beta_interactive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SocketIO : NSObject
+
+@end
